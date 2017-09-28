29576439cdc9026f91af4552752fe33fef7b2311
Piotr Jałocha <darricck04@gmail.com>
2017-09-28 14:44:10 +0200

    ver 1.0

---
+++ Column.js
@@ -0,0 +1,58 @@
+"use strict";
+
+function Column(id, name) {
+	var self = this;
+	this.id = id;
+    	this.name = name || 'Column';
+	this.element = createColumn();
+
+	function createColumn() {
+		var column = $('<div class="column"></div>'),
+			columnTitle = $('<h2 class="column-title">' + self.name + '</h2>'),
+			columnCardList = $('<ul class="card-list"></ul>'),
+			columnDelete = $('<button class="btn-delete">x</button>'),
+			columnAddCard = $('<button class="column-add-card">add card</button>');
+		
+		columnDelete.click(function() {
+			self.deleteColumn();
+		});
+		
+		columnAddCard.click(function(event) {
+	        	var cardName = prompt("Enter the name of the card");
+	        	event.preventDefault();
+	        	$.ajax({
+                		url: baseUrl + '/card',
+                		method: 'POST',
+                		data: {
+                    			name: cardName,
+                    			bootcamp_kanban_column_id: self.id
+                		},
+                		success: function(response) {
+                    			var card = new Card(response.id, cardName);
+                    			self.createCard(card);
+                		}
+            		});
+        	});
+			
+		column.append(columnTitle)
+			.append(columnDelete)
+			.append(columnAddCard)
+			.append(columnCardList);
+			return column;
+	}
+}
+Column.prototype = {
+	createCard: function(card) {
+		this.element.children('ul').append(card.element);
+	},
+	deleteColumn: function() {
+		var self = this;
+		$.ajax({
+			url: baseUrl + '/column/' + self.id,
+			method: 'DELETE',
+			success: function(response){
+				self.element.remove();
+			}
+		});
+	}
+};
